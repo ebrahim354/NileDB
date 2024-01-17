b2635495cec74540c2854eeaa1702d481ca7e441
Ebrahim <Ebrahim.elhabashy354@gmail.com>
2024-01-17 08:26:10 +0200

    Interducing The DiskManager class

--- main.cpp
+++ main.cpp
@@ -23,8 +23,6 @@ std::vector<std::string> strSplit(const std::string &str, char delimiter) {
     return (pieces);
 }
 
-
-
 bool areDigits(std::string nums){
     bool are_digits = true;
     for(size_t i = 0; i < nums.size(); i++){
@@ -36,7 +34,47 @@ bool areDigits(std::string nums){
 }
 
 
+class DiskManager {
+    public:
+        // returns 1 in case of failure and 0 in case of success.
+        int readFile(std::string file_name, std::vector<std::vector<std::string>> &ouput_buffer){
+            std::ifstream input_selected_table(file_name);
+
+            std::string row_buffer;
+            while(std::getline(input_selected_table, row_buffer)){
+                std::vector<std::string> columns = strSplit(row_buffer, ',');
+                ouput_buffer.emplace_back(columns);
+            }
+            if(input_selected_table.bad())
+                return 1;
+            input_selected_table.close();
+            return 0;
+        }
+
+        int writeFile(std::string file_name, std::vector<std::vector<std::string>> &input_buffer){
+            std::ofstream output_selected_table(file_name);
+            for(auto &row : input_buffer){
+                std::string line = "";
+                for(auto &col : row){
+                    line += col+",";        
+                }
+                output_selected_table << line << "\n";
+            }
+
+            if(output_selected_table.bad())
+                return 1;
+            output_selected_table.flush(); 
+            output_selected_table.close();
+            return 0;
+        }
+};
+
+
+
+
+
 int main() {
+    DiskManager DM;
     // setting up meta data.
     bool prompt_is_running = true;
     std::ifstream input_main_tables("main_tables.txt");
@@ -89,15 +127,17 @@ int main() {
         } else if(query_type == "SELECT" && tokens.size() == 3 && table_directory.count(tokens[2])){
             // SELECT FROM <table name> returns the entire table.
             std::string table_name = tokens[2];
-            std::ifstream input_selected_table(table_name + ".txt");
-            std::string row;
-            while(std::getline(input_selected_table, row)){
-                std::vector<std::string> columns = strSplit(row, ',');
-                for(size_t i = 0; i < columns.size(); i++)
-                    std::cout << columns[i] << " " ;
-                std::cout << std::endl;
+            std::string file_name = table_name+".txt";
+
+            std::vector<std::vector<std::string>> table_buffer;
+            int err = DM.readFile(file_name, table_buffer);
+            if(!err){
+                for(auto &row : table_buffer){
+                    for(auto &col : row)
+                        std::cout << col << " " ;
+                    std::cout << "\n";
+                }
             }
-            input_selected_table.close();
             valid_query = true;
         } else if(query_type == "SELECT" 
                 && tokens.size() == 7 && table_directory.count(tokens[2])
@@ -106,48 +146,53 @@ int main() {
                 && tokens[5] == "="){
             // SELECT FROM <table name> WHERE <column number> = "STRING LITERAL".
             std::string table_name = tokens[2];
+            std::string file_name = table_name+".txt";
+            int filter_column_num = std::stoi(tokens[4]);
             std::string filter_str = tokens[6];
-            size_t filter_column_num = std::stoi(tokens[4]);
-
             std::vector<std::vector<std::string>> table_buffer;
-            std::string row_buffer;
-            
-            std::ifstream input_selected_table(table_name + ".txt");
-            while(std::getline(input_selected_table, row_buffer)){
-                std::vector<std::string> columns = strSplit(row_buffer, ',');
-                table_buffer.emplace_back(columns);
-                
-            }
-            input_selected_table.close();
 
-            for(auto &row: table_buffer){
-                if(row[filter_column_num-1] == filter_str){
-                    for(size_t i = 0; i < row.size(); i++)
-                        std::cout << row[i] << " " ;
-                    std::cout << std::endl;
+            int err = DM.readFile(file_name, table_buffer);
+            if(!err){
+                for(auto &row : table_buffer){
+                    if(row[filter_column_num-1] == filter_str){
+                        for(size_t i = 0; i < row.size(); i++)
+                            std::cout << row[i] << " " ;
+                        std::cout << std::endl;
+                    }
                 }
             }
             valid_query = true;
         } else if(query_type == "DELETE" && tokens.size() == 3 && table_directory.count(tokens[2])){
             // DELETE FROM <table name> deletes the entire table.
             std::string table_name = tokens[2];
-            std::ofstream output_selected_table(table_name + ".txt", std::ios::trunc);
-            output_selected_table.close();
-            std::cout << "CLEARED TABLE" << std::endl;
-            valid_query = true;
+            std::string file_name = table_name+".txt";
+            std::vector<std::vector<std::string>> tmp_buffer;
+            int err = DM.writeFile(file_name, tmp_buffer);
+            if(!err){
+                std::cout << "CLEARED TABLE" << std::endl;
+                valid_query = true;
+            }
         } else if(query_type == "INSERT" && tokens.size() >= 3 
                 && table_directory.count(tokens[2]) && table_directory[tokens[2]].num_of_columns == tokens.size() - 3
                 ){
             // INSERT INTO <table name> <n values> and n = number of columns.
             std::string table_name = tokens[2];
+            std::string file_name = table_name+".txt";
             std::size_t n = table_directory[table_name].num_of_columns;
+            std::vector<std::vector<std::string>> io_buffer;
             std::string row = ""; 
-            for(size_t i = 0; i < n; i++)  row += tokens[i+3] + ',';
-            std::ofstream output_selected_table(table_name + ".txt", std::ios::app | std::ios::ate);
-            output_selected_table << row << std::endl;
-            output_selected_table.close();
-            std::cout << "INSERTED A NEW RECORD" << std::endl;
-            valid_query = true;
+
+            int r_err = DM.readFile(file_name, io_buffer);
+            if(!r_err){
+                std::vector<std::string> row;
+                for(size_t i = 0; i < n; i++)  row.push_back(tokens[i+3]);
+                io_buffer.push_back(row);
+                int w_err = DM.writeFile(file_name, io_buffer);
+                if(!w_err){
+                    std::cout << "INSERTED A NEW RECORD" << std::endl;
+                    valid_query = true;
+                }
+            }
         }
         
         if(!valid_query){
