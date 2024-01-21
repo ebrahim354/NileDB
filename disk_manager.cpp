c50c7aecc27b0a0cc8b7392cd4ab6bdb414a5fce
Ebrahim <Ebrahim.elhabashy354@gmail.com>
2024-01-21 12:07:57 +0200

    Adding Multi file support : DiskManager

--- disk_manager.cpp
+++ disk_manager.cpp
@@ -4,72 +4,106 @@
 #include <fstream>
 #include <cstdint>
 #include <string.h>
+#include <sys/types.h>
+#include <unordered_map>
 
-typedef uint32_t page_id_t;
+
+struct PageID{
+    std::string file_name_{0};
+    int32_t page_num_{0};
+};
+
 
 #define PAGE_SIZE 4096 //4KB
-#define INVALID_PAGE_ID -1
-#define DIRECTORY_PAGE_ID 0
+#define FILE_EXT ".NDB"
+const PageID INVALID_PAGE_ID = PageID{ .file_name_ = "INVALID_FILE_NAME", .page_num_ = -1 };
+
+
 
 
 struct Page {
         char* data_[PAGE_SIZE]{};
-        page_id_t page_id_ = INVALID_PAGE_ID;
+        PageID page_id_ = INVALID_PAGE_ID;
 };
 
 
 class DiskManager {
     public:
-
-        DiskManager(std::string main_db_file): main_db_file_(main_db_file){
-            main_db_stream_.open(main_db_file_, std::ios::binary | std::ios::in | std::ios::out);
-            if (!main_db_stream_.is_open()) {
-                main_db_stream_.clear();
-                main_db_stream_.open(main_db_file_, std::ios::binary | std::ios::trunc | std::ios::out | std::ios::in);
+        DiskManager();
+        ~DiskManager(){
+            for(auto &file : cached_files_){
+                file.second.close();
             }
         }
-        ~DiskManager(){
-            main_db_stream_.close();
-        }
-
+        // 1 on failure, 0 on success.
+        int openFile(std::string file_name);
         // returns 1 in case of failure and 0 in case of success.
-        int readPage(page_id_t page_id, char* ouput_buffer);
-        int writePage(page_id_t page_id, char* input_buffer);
+        int readPage(PageID page_id, char* ouput_buffer);
+        int writePage(PageID page_id, char* input_buffer);
+        
     private:
-        std::fstream main_db_stream_;
-        std::string main_db_file_;
+        std::unordered_map<std::string, std::fstream> cached_files_;
 };
 
-int DiskManager::readPage(page_id_t page_id, char* output_buffer){
-    int offset = page_id * PAGE_SIZE;
 
-    main_db_stream_.seekp(offset);
-    main_db_stream_.read(output_buffer, PAGE_SIZE);
-    if (main_db_stream_.bad()) {
-        std::cerr << "Read page error: bad file format" << std::endl;
-        return 1;
-    }
+int DiskManager::readPage(PageID page_id, char* output_buffer) {
+    std::string file_name =  page_id.file_name_;
+    uint32_t page_num = page_id.page_num_;
+    int offset = page_num * PAGE_SIZE;
+    int err = openFile(file_name);
+    if(err) return 1;
+    auto file_stream = &cached_files_[file_name];
 
-    int read_count = main_db_stream_.gcount();
+    file_stream->seekp(offset);
+    file_stream->read(output_buffer, PAGE_SIZE);
+
+    int read_count = file_stream->gcount();
     if (read_count < PAGE_SIZE) {
         std::cerr << "Read page error: invalid read count" << std::endl;
-        main_db_stream_.clear();
         memset(output_buffer + read_count, 0, PAGE_SIZE - read_count);
         return 1;
     }
     return 0;
 }
 
+int DiskManager::writePage(PageID page_id, char* input_buffer) {
+    std::string file_name =  page_id.file_name_;
+    uint32_t page_num = page_id.page_num_;
+    int offset = page_num * PAGE_SIZE;
+    int err = openFile(file_name);
+    if(err) return 1;
 
-int DiskManager::writePage(page_id_t page_id, char* input_buffer){
-    int offset = page_id * PAGE_SIZE;
-    main_db_stream_.seekp(offset);
-    main_db_stream_.write(input_buffer, PAGE_SIZE);
-    if (main_db_stream_.bad()) {
+    auto file_stream = &cached_files_[file_name];
+    file_stream->seekp(offset);
+    file_stream->write(input_buffer, PAGE_SIZE);
+    if (file_stream->bad()) {
         std::cerr << "I/O error while writing" << std::endl;
         return 1;
     }
-    main_db_stream_.flush();
+    file_stream->flush();
     return 0;
 }
+
+int DiskManager::openFile(std::string file_name){
+    // bad file format.
+    std::string::size_type n = file_name.rfind(FILE_EXT);
+    if (n == std::string::npos) 
+        return 1;
+    // cache miss
+    if (!cached_files_.count(file_name)) 
+        cached_files_[file_name] = std::fstream(file_name, std::ios::binary | std::ios::app | std::ios::out | std::ios::in);
+    // file doesn't exist.
+    // create a new one and return 1 on failure.
+    if(!cached_files_[file_name].is_open()){
+        cached_files_[file_name].open(file_name, std::ios::binary | std::ios::trunc | std::ios::out | std::ios::in);
+        if (cached_files_[file_name].is_open()) {
+            cached_files_.erase(file_name);
+            return 1;
+        }
+         
+    }
+    return 0;
+}
+
+
 
